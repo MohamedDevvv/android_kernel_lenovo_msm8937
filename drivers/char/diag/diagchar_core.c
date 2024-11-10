00690708e7d34971372b1d025e18dab336a291b8
MohamedDevvv <aaamohamed499@gmail.com>
2024-11-10 12:00:19 +0200

    Fixing

--- drivers/char/diag/diagchar_core.c
+++ drivers/char/diag/diagchar_core.c
@@ -1841,30 +1841,22 @@ static int diag_ioctl_lsm_deinit(void)
 		mutex_unlock(&driver->diagchar_mutex);
 		return -EINVAL;
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 	if (!(driver->data_ready[i] & DEINIT_TYPE)) {
 		driver->data_ready[i] |= DEINIT_TYPE;
 		atomic_inc(&driver->data_ready_notif[i]);
 	}
-||||||| 68ab5f46024e
 
 	driver->data_ready[i] |= DEINIT_TYPE;
-=======
-
-	driver->data_ready[i] |= DEINIT_TYPE;
-	atomic_inc(&driver->data_ready_notif[i]);
->>>>>>> 8338cb54736192dbbac3e478932c717127e54cf7
-||||||| 233870be3b30
 
 	driver->data_ready[i] |= DEINIT_TYPE;
 	atomic_inc(&driver->data_ready_notif[i]);
-=======
+
+	driver->data_ready[i] |= DEINIT_TYPE;
+	atomic_inc(&driver->data_ready_notif[i]);
 	if (!(driver->data_ready[i] & DEINIT_TYPE)) {
 		driver->data_ready[i] |= DEINIT_TYPE;
 		atomic_inc(&driver->data_ready_notif[i]);
 	}
->>>>>>> 8f397ec8a923a04912fd644b6195daf04efb86de
 	mutex_unlock(&driver->diagchar_mutex);
 	wake_up_interruptible(&driver->wait_q);
 
