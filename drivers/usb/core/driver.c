2a12fa9d8ef454d5bc7ee762e5076fe0e3d0dc7d
MohamedDevvv <aaamohamed499@gmail.com>
2024-11-11 06:44:22 +0200

    Update driver.c

--- drivers/usb/core/driver.c
+++ drivers/usb/core/driver.c
@@ -501,17 +501,7 @@ int usb_driver_claim_interface(struct usb_driver *driver,
 	struct device *dev;
 	struct usb_device *udev;
 	int retval = 0;
-<<<<<<< HEAD
-	int lpm_disable_error;
-||||||| merged common ancestors
-<<<<<<<<< Temporary merge branch 1
-	int lpm_disable_error;
-||||||||| a5f9be3576c3
 	int lpm_disable_error = -ENODEV;
-=========
->>>>>>>>> Temporary merge branch 2
-=======
->>>>>>> a5f3199c3d7940e68405c141c48533581680743f
 
 	if (!iface)
 		return -ENODEV;
@@ -528,7 +518,6 @@ int usb_driver_claim_interface(struct usb_driver *driver,
 
 	iface->condition = USB_INTERFACE_BOUND;
 
-<<<<<<< HEAD
 	/* Disable LPM until this driver is bound. */
 	lpm_disable_error = usb_unlocked_disable_lpm(udev);
 	if (lpm_disable_error && driver->disable_hub_initiated_lpm) {
@@ -537,17 +526,6 @@ int usb_driver_claim_interface(struct usb_driver *driver,
 		return -ENOMEM;
 	}
 
-||||||| merged common ancestors
-<<<<<<<<< Temporary merge branch 1
-	/* Disable LPM until this driver is bound. */
-	lpm_disable_error = usb_unlocked_disable_lpm(udev);
-	if (lpm_disable_error && driver->disable_hub_initiated_lpm) {
-		dev_err(&iface->dev, "%s Failed to disable LPM for driver %s\n.",
-				__func__, driver->name);
-		return -ENOMEM;
-	}
-
-||||||||| a5f9be3576c3
 	/* See the comment about disabling LPM in usb_probe_interface(). */
 	if (driver->disable_hub_initiated_lpm) {
 		lpm_disable_error = usb_unlocked_disable_lpm(udev);
@@ -558,10 +536,6 @@ int usb_driver_claim_interface(struct usb_driver *driver,
 		}
 	}
 
-=========
->>>>>>>>> Temporary merge branch 2
-=======
->>>>>>> a5f3199c3d7940e68405c141c48533581680743f
 	/* Claimed interfaces are initially inactive (suspended) and
 	 * runtime-PM-enabled, but only if the driver has autosuspend
 	 * support.  Otherwise they are marked active, to prevent the
